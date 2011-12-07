bea29b7b393f6819ada3b9879d43fa803518863b
Chet Ramey <chet.ramey@case.edu>
2011-12-07 09:26:50 -0500

    commit bash-20080807 snapshot

--- lib/readline/readline.h
+++ lib/readline/readline.h
@@ -40,9 +40,9 @@ extern "C" {
 #endif
 
 /* Hex-encoded Readline version number. */
-#define RL_READLINE_VERSION	0x0502		/* Readline 5.2 */
-#define RL_VERSION_MAJOR	5
-#define RL_VERSION_MINOR	2
+#define RL_READLINE_VERSION	0x0600		/* Readline 6.0 */
+#define RL_VERSION_MAJOR	6
+#define RL_VERSION_MINOR	0
 
 /* Readline data structures. */
 
