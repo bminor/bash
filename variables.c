4b82d1cde775948a013812745b0753fef761e33d
Chet Ramey <chet.ramey@case.edu>
2012-01-09 08:33:08 -0500

    commit bash-20111118 snapshot

--- variables.c
+++ variables.c
@@ -535,11 +535,6 @@ initialize_shell_variables (env, privmode)
 
       set_if_not ("HISTFILE", name);
       free (name);
-
-#if 0
-      set_if_not ("HISTSIZE", "500");
-      sv_histsize ("HISTSIZE");
-#endif
     }
 #endif /* HISTORY */
 
@@ -4487,14 +4482,16 @@ sv_histsize (name)
       if (legal_number (temp, &num))
 	{
 	  hmax = num;
-	  if (name[4] == 'S')
+	  if (hmax < 0 && name[4] == 'S')
+	    unstifle_history ();	/* unstifle history if HISTSIZE < 0 */
+	  else if (name[4] == 'S')
 	    {
 	      stifle_history (hmax);
 	      hmax = where_history ();
 	      if (history_lines_this_session > hmax)
 		history_lines_this_session = hmax;
 	    }
-	  else
+	  else if (hmax >= 0)	/* truncate HISTFILE if HISTFILESIZE >= 0 */
 	    {
 	      history_truncate_file (get_string_value ("HISTFILE"), hmax);
 	      if (hmax <= history_lines_in_file)
