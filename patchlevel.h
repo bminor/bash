489302a91dd6ce2dbb6ec41fd92872dd5ac092ce
Chet Ramey <chet.ramey@case.edu>
2016-06-20 15:14:20 -0400

    Bash-4.3 patch 44

--- patchlevel.h
+++ patchlevel.h
@@ -25,6 +25,6 @@
    regexp `^#define[ 	]*PATCHLEVEL', since that's what support/mkversion.sh
    looks for to find the patch level (for the sccs version string). */
 
-#define PATCHLEVEL 43
+#define PATCHLEVEL 44
 
 #endif /* _PATCHLEVEL_H_ */
