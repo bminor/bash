591b09c48ffef7e5aeaa28d1fb5181f8dbe66905
Chet Ramey <chet.ramey@case.edu>
2016-06-20 15:14:32 -0400

    Bash-4.3 patch 45

--- redir.c
+++ redir.c
@@ -671,7 +671,10 @@ redir_open (filename, flags, mode, ri)
 	  fd = open (filename, flags, mode);
 	  e = errno;
 	  if (fd < 0 && e == EINTR)
-	    QUIT;
+	    {
+	      QUIT;
+	      run_pending_traps ();
+	    }
 	  errno = e;
 	}
       while (fd < 0 && errno == EINTR);
