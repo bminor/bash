1ebae4666c585ad9573cccde5c2b697d72363a9b
Chet Ramey <chet.ramey@case.edu>
2017-09-20 09:30:30 -0400

    commit bash-20170915 snapshot

--- shell.c
+++ shell.c
@@ -1,6 +1,6 @@
 /* shell.c -- GNU's idea of the POSIX shell specification. */
 
-/* Copyright (C) 1987-2017 Free Software Foundation, Inc.
+/* Copyright (C) 1987-2015 Free Software Foundation, Inc.
 
    This file is part of GNU Bash, the Bourne Again SHell.
 
