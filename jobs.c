3ba697465bc74fab513a26dea700cc82e9f4724e
Chet Ramey <chet.ramey@case.edu>
2019-04-20 14:27:56 -0400

    Bash-5.0 patch 7: fix exec builtin leaving the terminal in the wrong process group

--- jobs.c
+++ jobs.c
@@ -4837,15 +4837,13 @@ void
 end_job_control ()
 {
   if (job_control)
-    {
-      terminate_stopped_jobs ();
+    terminate_stopped_jobs ();
 
-      if (original_pgrp >= 0)
-	give_terminal_to (original_pgrp, 1);
-    }
+  if (original_pgrp >= 0 && terminal_pgrp != original_pgrp)
+    give_terminal_to (original_pgrp, 1);
 
-  if (original_pgrp >= 0)
-    setpgid (0, original_pgrp);
+  if (original_pgrp >= 0 && setpgid (0, original_pgrp) == 0)
+    shell_pgrp = original_pgrp;
 }
 
 /* Restart job control by closing shell tty and reinitializing.  This is
