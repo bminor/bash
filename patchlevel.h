a78e27a142ef7949dbc8111b1a7bd2a17697cb9f
Chet Ramey <chet.ramey@case.edu>
2023-11-09 16:44:58 -0500

    Bash-5.2 patch 18: fix for returning unknown tokens to the bison parser

--- patchlevel.h
+++ patchlevel.h
@@ -25,6 +25,6 @@
    regexp `^#define[ 	]*PATCHLEVEL', since that's what support/mkversion.sh
    looks for to find the patch level (for the sccs version string). */
 
-#define PATCHLEVEL 17
+#define PATCHLEVEL 18
 
 #endif /* _PATCHLEVEL_H_ */
