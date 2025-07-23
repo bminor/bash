2ea3d6064911104d8e0437cb75b0e2559b0cd1b2
Chet Ramey <chet.ramey@case.edu>
2025-07-23 15:47:12 -0400

    Bash-5.3 patch 2: do not try to use shm_open, there is too much variance in behavior across systems

--- patchlevel.h
+++ patchlevel.h
@@ -25,6 +25,6 @@
    regexp `^#define[ 	]*PATCHLEVEL', since that's what support/mkversion.sh
    looks for to find the patch level (for the sccs version string). */
 
-#define PATCHLEVEL 1
+#define PATCHLEVEL 2
 
 #endif /* _PATCHLEVEL_H_ */
